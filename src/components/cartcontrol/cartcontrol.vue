51bdf5dc3a862d87d64b9ce9a76ebc301846f2db
ChenYCL <aa34913@126.com>
2017-08-01 19:13:16 +0800

    'update'

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="cartcontrol">
+    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreseCount($event)" transition="move">
+      <!--内层滚动-->
+      <span class="inner icon-remove_circle_outline"></span>
+    </div>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click="addCart($event)"></div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  // 观察全局变量的变化，需要引入Vue插件 ，调用接口
+  import Vue from 'vue';
+
+  export default {
+//    接收一个商品
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        ;
+        if (!this.food.count) {
+          // set方法 类似java中 setter 和getter 用静态的方法取修改里面的数值  Vue.set(object, key, value)： 使用Vue.set全局观测到
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+      },
+      decreseCount(event) {
+        if (!event._constructed) {
+          return;
+        }
+        ;
+        if (this.food.count > 0) {
+          this.food.count--;
+        }
+      }
+    }
+  };
+</script>
+<style lang='stylus' rel='stylesheet/stylus' type="text/stylus">
+  .cartcontrol
+    font-size 0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      // 设置动画
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3D(0,0,0)
+        .inner
+          display: inline-block
+          font-size 24px
+          color rgb(0, 160, 220)
+          font-weight: 700
+          line-height 24px
+          transition: all 0.4s linear
+          // 滚动触发设置
+          transform: rotate(0)
+      &.move-enter,&.move-leave
+        opacity 0
+        // 3d 设置坐标  x,y,z
+        transform: translate3D(24px,0,0)
+        .inner
+          transform:rotate(180deg)
+    .cart-count
+      display: inline-block
+      vertical-align top
+      padding-top 6px
+      line-height 24px
+      text-align center
+      font-size 10px
+      color rgb(0, 160, 220)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      font-size 24px
+      color rgb(0, 160, 220)
+      font-weight: 700
+      line-height 24px
+</style>
+<!--这里定义的购物添加组件在 商品父组件中引入使用-->
---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="shopcart">
+    <!--底部的区块-->
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo"  :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+          </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
+        </div>
+        <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}</div>
+        <div class="desc">另需配送￥{{deliveryPrice}}元</div>
+      </div>
+      <div class="content-right">
+        <div class="pay" :class="payClass">
+          {{payDesc}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+//      由goods组件告诉我们 选择了那种商品
+      selectFoods: {
+        type: Array,
+        default() {
+          return [
+            {
+              price: 10,
+              count: 2
+            }
+          ];
+        }
+      },
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      }
+    },
+//    计算选择商品的总价
+    computed: {
+//      计算总价的方法
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
+//      计算所有商品的总和
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `差${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}元起送`;
+        } else {
+          return '去结算';
+        }
+      },
+      payClass() {
+        if (this.totalPrice === 0) {
+          return 'not-enough';
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
+      }
+    }
+  };
+</script>
+<style lang='stylus' rel='stylesheet/stylus' type="text/stylus">
+  .shopcart
+    position: fixed
+    left: 0
+    bottom: 0
+    z-index: 50
+    width: 100%
+    height: 48px
+    .content
+      display flex
+      background-color #161C29
+      font-size 0
+      .content-left
+        flex: 1
+        /*购物车logo的容器*/
+        .logo-wrapper
+          display: inline-block
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding 6px
+          width: 56px
+          height: 56px
+          box-sizing border-box
+          /*顶部对齐*/
+          vertical-align top
+          border-radius: 50%
+          background-color #141d27
+          .logo
+            width: 100%
+            height 100%
+            border-radius 50%
+            background-color: #2B343C
+            /*给里面的字体图标 居中效果*/
+            text-align center
+            &.highlight
+              background-color rgb(0, 160, 220)
+            .icon-shopping_cart
+              font-size 24px
+              line-height 44px
+              color: #80858A
+              &.highlight
+                color: #fff
+          .num
+            position absolute
+            top 0
+            right 0
+            width 24px
+            height 16px
+            line-height 16px
+            text-align center
+            border-radius 16px
+            font-size 9px
+            font-weight 700
+            color: rgb(255, 255, 255)
+            background-color rgb(240, 20, 20)
+            box-shadow 0px 4px 8px 0px rgba(0, 0, 0, 0.4)
+        /*价格*/
+        .price
+          display: inline-block
+          vettical-align: top
+          margin-top: 12px
+          padding-right 12px
+          box-sizing border-box
+          border-right 1px solid rgba(255, 255, 255, 0.1)
+          font-size 16px
+          font-weight 700
+          line-height 24px
+          color: rgba(255, 255, 255, 0.4)
+          &.highlight
+            color: #fff
+        .desc
+          display: inline-block
+          vettical-align: top
+          margin-top: 12px
+          margin-right 12px
+          line-height 24px
+          font-size 16px
+          font-weight 100
+          color: rgba(255, 255, 255, 0.4)
+      .content-right
+        flex 0 0 105px
+        width 105px
+        background: #2B333B
+        .pay
+          font-size 13px
+          font-weight 700
+          text-align center
+          height 48px
+          line-height 48px
+          color: rgba(255, 255, 255, 0.4)
+          &.not-enough
+            background-color #2B333B
+          &.enough
+            background-color #00b43c
+            color: #fff
+</style>
