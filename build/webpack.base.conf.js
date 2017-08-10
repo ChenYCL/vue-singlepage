a1a67e297e825e31a7b5d764332fe911388ee676
ChenYCL <aa34913@126.com>
2017-08-10 09:27:48 +0800

    'update'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -78,9 +78,9 @@ module.exports = {
       }
     ]
   },
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
+  //eslint: {
+  //  formatter: require('eslint-friendly-formatter')
+  //},
   vue: {
     loaders: utils.cssLoaders()
   }
